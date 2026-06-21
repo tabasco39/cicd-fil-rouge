38781a27ee545554728600a9c7572c28de82b728
Antsa <ranaivoantsaa@gmail.com>
2026-06-21 16:51:33 +0300

    modif text

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(
         JSON.stringify({
-            message: "CI/CD fil ruge en ligne",
+            message: "CI/CD fil rouge en ligne",
             resultat: add(2, 3)
         })
     )
